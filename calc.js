a5b4a7ca86ccbcf8e25370817726e24188d438a5
jaatuw <hhdeamorim@gmail.com>
2025-04-21 18:56:26 -0300

    final version 1.0
    Co-authored-by: Lucas Vinicius <lucasviniciusdacosta2006@gmail.com>

--- calc.js
+++ calc.js
@@ -5,46 +5,72 @@ var b = '';
 var sinal = '';
 var r;
 
-function clearall() {
+function dec() {
+    if (resultado.innerHTML.includes('.')) {
+        
+    } else {
+        resultado.innerHTML = resultado.innerHTML + ".";
+        sinal = '';
+    }
+}
+function deleter() {
+    if (resultado.innerHTML.length > 1){
+    resultado.innerHTML = resultado.innerHTML.slice(0, -1);
+} else if(resultado.innerHTML.length == 1) {
     resultado.innerHTML = 0;
-
+}
 }
 
+function clearall() {
+    resultado.innerHTML = 0;
+    a = '';
+    b = '';
+    sinal = '';
+    resultado1 = '';
+}
+function check() {
+    if (sinal !== '=' && sinal !== '' && b !== '' && a !== '') {
+        operate();
+    }
+}
 function soma() {
+    check();
     sinal = '+';
-    clearall();
 }   
 
-
 function sub() {
+    check();
     sinal = '-'
-    clearall();
 }
 
 function mult() {
+    check();
     sinal = '*'
-    clearall();
 }
 
 function div() {
-
+    check();
     sinal = '/'
-    clearall();
 }
+function modul() {
+    check();
+    sinal = '%'
+}   
 
 function number(x) {
 
     if (sinal == '='){
         resultado.innerHTML = x;
-        sinal = '';
+        sinal = ''; 
+    } else if (resultado.innerHTML === a && sinal !== '') {
+        resultado.innerHTML = x;
     }
-    else if (resultado.innerHTML == 0 ) {
+    else if (resultado.innerHTML == 0  && resultado.innerHTML !== "0.") {
         resultado.innerHTML = x;
     }
     else if (resultado.innerHTML <= 9999999999) {
         resultado.innerHTML += x;
     }
-
     if (sinal === '') {
         a = resultado.innerHTML;
     } else {
@@ -58,25 +84,32 @@ function number(x) {
 function operate() {
     let p = parseFloat(a);
     let q = parseFloat(b);
+    var resultado1;
     switch (sinal) {
         case '+':
-            resultado.innerHTML = p + q;
+            resultado1 = p + q;
+            resultado.innerHTML = parseFloat(resultado1.toPrecision(8));
             break;
         case '-':
-            resultado.innerHTML = p - q;
+            resultado1 = p - q;
+            resultado.innerHTML = parseFloat(resultado1.toPrecision(8));
             break;
         case '*':
-            resultado.innerHTML = p * q;
+            resultado1 = p * q;
+            resultado.innerHTML = parseFloat(resultado1.toPrecision(8));
             break;
         case '/':
             if (q != 0){
-                resultado.innerHTML = p / q;
+                resultado1 = p / q;
+                resultado.innerHTML = parseFloat(resultado1.toPrecision(8));
             } else resultado.innerHTML = "ERROR"; 
             break;
         case '%':
             resultado.innerHTML = p % q;
             break;
            }
+        
     sinal = '=';
     a = resultado.innerHTML;
+    b = '';
 }
